dad9de1aef6089cba90a077d401e51744dd1e261
Adalynity <adalynity@gmail.com>
2024-06-04 18:41:47 +0100

    Added navbar to contacts

--- Assets/JavaScript Files/contact.js
+++ Assets/JavaScript Files/contact.js
@@ -1,3 +1,107 @@
+// Some small animations
+function onHoverImg()
+{
+    $("#logoImg").attr('src', 'Assets/Images/default_logo_white.png');
+}
+
+function offHoverImg()
+{
+    $("#logoImg").attr('src', 'Assets/Images/default_logo.png');
+}
+
+function onHoverBell()
+{
+    if(document.getElementById('bellConcierge').classList.contains('fa-shake')){
+
+    }
+    else{
+        document.getElementById('bellConcierge').classList.add('fa-shake');
+    }
+    
+}
+function offHoverBell()
+{
+    if(document.getElementById('bellConcierge').classList.contains('fa-shake')){
+        document.getElementById('bellConcierge').classList.remove('fa-shake');
+    }
+    else{
+
+    }
+    
+}
+
+function onHoverLocation(){
+    if(document.getElementById('locationDot').classList.contains('fa-bounce')){
+
+    }
+    else{
+        document.getElementById('locationDot').classList.add('fa-bounce');
+    };
+}
+
+function offHoverLocation(){
+    if(document.getElementById('locationDot').classList.contains('fa-bounce')){
+        document.getElementById('locationDot').classList.remove('fa-bounce');
+    }
+    else{
+        
+    };
+}
+
+function onHoverShop(){
+    if(document.getElementById('shoppingCart').classList.contains('fa-flip')){
+        
+    }
+    else{
+        document.getElementById('shoppingCart').classList.add('fa-flip');
+    }
+}
+
+function offHoverShop(){
+    if(document.getElementById('shoppingCart').classList.contains('fa-flip')){
+        document.getElementById('shoppingCart').classList.remove('fa-flip');
+    }
+    else{
+        
+    }
+}
+
+function onHoverMail(){
+    if(document.getElementById('mailIcon').classList.contains('fa-beat-fade')){
+        
+    }
+    else{
+        document.getElementById('mailIcon').classList.add('fa-beat-fade');
+    }
+}
+
+function offHoverMail(){
+    if(document.getElementById('mailIcon').classList.contains('fa-beat-fade')){
+        document.getElementById('mailIcon').classList.remove('fa-beat-fade');
+    }
+    else{
+        
+    }
+}
+
+function onHoverHome(){
+    if(document.getElementById('homeIcon').classList.contains('fa-beat')){
+        
+    }
+    else{
+        document.getElementById('homeIcon').classList.add('fa-beat');
+    }
+}
+
+function offHoverHome(){
+    if(document.getElementById('homeIcon').classList.contains('fa-beat')){
+        document.getElementById('homeIcon').classList.remove('fa-beat');
+    }
+    else{
+        
+    }
+}
+
 document.getElementById('contactForm').addEventListener('submit', function(event) {
     event.preventDefault();
 
